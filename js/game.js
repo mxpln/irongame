6ceb625e958249c78dd1fb916a4874de9fa8cc42
Maxime Paolone <paolonemax@gmail.com>
2020-04-07 17:58:28 +0200

    Collision

--- js/game.js
+++ js/game.js
@@ -1,10 +1,10 @@
 const box = document.querySelector(".box");
+const player = document.querySelector(".player");
 const box2 = document.querySelector(".box2");
 
 function movePlayer() {
   document.addEventListener("keydown", move);
   function move(e) {
-    let player = document.querySelector(".player");
     switch (e.code) {
       case "KeyW":
         player.classList.add("is-jumping");
@@ -29,16 +29,18 @@ function boxMove() {
   if (x < -200) {
     x = 800;
   }
+  if (checkBoops(player.getBoundingClientRect(), box.getBoundingClientRect())) {
+    console.log("I am colliding !!");
+  }
 }
-
-// start();
-// sendNewBoxes();
-// test
-
-// once box has left the scene/player dodge the box
-// isBoxDead()
-
-// while player still alive
-// sendNewBoxes()
-
-// collision()
+function checkBoops(rect1, rect2) {
+  if (
+    rect1.x < rect2.x + rect2.width &&
+    rect1.x + rect1.width > rect2.x &&
+    rect1.y < rect2.y + rect2.height &&
+    rect1.y + rect1.height > rect2.y
+  ) {
+    return true;
+  }
+  return false;
+}
